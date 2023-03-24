81f5ec71fc18d7db69c8bf68f0a8d0e4896e0f1f
Owen Wang <owang420@gmail.com>
2023-03-24 10:03:49 -0400

    Added documentation Renamed from ComputerInformation.java

---
+++ src/ComputerWord.java
@@ -0,0 +1,34 @@
+/*
+ * ComputerGuess.java
+ * ComputerGuess class determines the computer generated word
+ * Owen Wang
+ * Last Edited 24/03/2023
+ */
+public class ComputerWord {
+
+    String word;
+
+    /**
+     * Constructor
+     * Gives word a default value
+     */
+    ComputerWord(){
+        word = "";
+    }
+
+    /**
+     * Randomly generates the value of a word from a list of valid words
+     */
+    public void setWord() {
+        //AHHHH
+    }
+
+    /**
+     * Returns the value of the computer generated word
+     *
+     * @return - computer generated word
+     */
+    public String getWord(){
+        return word;
+    }
+}
