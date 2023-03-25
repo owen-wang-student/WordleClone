0da60d6728d38082edebb4ccd941aec7ee09ba2a
Owen Wang <owang420@gmail.com>
2023-03-25 13:24:13 -0400

    Added computer generated word

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        UserGuess NewWordle = new UserGuess();
+        UserGuess guess = new UserGuess();
+        ComputerWord word = new ComputerWord();
     }
 }
